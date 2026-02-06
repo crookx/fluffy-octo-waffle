1f44766402e8eced505e8d6aad02fe2559f4e2e8
Kyaka <kyaka908@gmail.com>
2026-02-06 14:21:27 +0000

    change colour (_for element <Button>_)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -147,7 +147,7 @@ export default function SignupPage() {
                   </FormItem>
                 )}
               />
-              <Button type="submit" className="w-full" disabled={isSubmitting}>
+              <Button variant="accent" type="submit" className="w-full" disabled={isSubmitting}>
                 {isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
                 Create Account
               </Button>
