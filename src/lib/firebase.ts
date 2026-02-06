4da8ea7de9fc554af75c1a6494416988a93487a2
Kyaka <kyaka908@gmail.com>
2026-02-06 16:31:19 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,27 +3,35 @@
 import {initializeApp, getApp, getApps, FirebaseApp} from 'firebase/app';
 import {getAuth, Auth} from 'firebase/auth';
 import {getFirestore, Firestore} from 'firebase/firestore';
+import { getAnalytics, Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  "projectId": "studio-5381270789-53c19",
-  "appId": "1:222004652932:web:661421367be7ba80b10bd1",
-  "apiKey": "AIzaSyDPh2FY2dXI4gTR9n1N3PCkVn1497kpUJs",
-  "authDomain": "studio-5381270789-53c19.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "222004652932",
-  "storageBucket": "studio-5381270789-53c19.appspot.com"
+  apiKey: "AIzaSyDETO0ohxe5Hmu5XBoWwZrnGbLNQ5fYdTk",
+  authDomain: "kenya-land-trust.firebaseapp.com",
+  projectId: "kenya-land-trust",
+  storageBucket: "kenya-land-trust.appspot.com",
+  messagingSenderId: "390036863335",
+  appId: "1:390036863335:web:295ecb4c46f298aa5597c2",
+  measurementId: "G-8TEKGJSJD1"
 };
 
 let app: FirebaseApp;
 let auth: Auth;
 let db: Firestore;
+let analytics: Analytics | undefined = undefined;
 
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
+  if (typeof window !== 'undefined') {
+    analytics = getAnalytics(app);
+  }
 } else {
   app = getApp();
+  if (typeof window !== 'undefined') {
+     analytics = getAnalytics(app);
+  }
 }
 auth = getAuth(app);
 db = getFirestore(app);
 
-export {app, auth, db};
+export {app, auth, db, analytics};
